3e38a6c32007ad68d01b950dd53f4efabe324a19
eline-91 <elinevanelburg@hotmail.com>
2016-01-13 16:57:46 +0100

    Rathee_Elburg: Made the linear model, compared the maps and calculated RMSE.

--- main.R
+++ main.R
@@ -20,12 +20,27 @@ for(i in 1:length(gewata_list)){
 vcfGewata[vcfGewata > 100] <- NA
 alldata <- brick(GewataB1, GewataB2, GewataB3, GewataB4, GewataB5, GewataB7, vcfGewata)
 names(alldata) <- c("band1", "band2", "band3", "band4", "band5", "band7", "VCF")
-
+plot(alldata, col=)
 # Extract all data to a dataframe
 df <- as.data.frame(getValues(alldata))
 
-# Plot
+# Make plots for every band against the VCF layer
 for(i in 1:6){
 	pairs(alldata[[c(i,7)]], maxpixels=10000)
 }
 
+# Make the linear model with interactions and without interactions
+model <- lm(VCF ~ band1 + band2 + band3 + band4 + band5, data = df)
+
+# Predict the map
+vcfMap <- predict(alldata[[1:5]], model = model)
+
+# Change negative values to 0 because VCF can just be between 0 and 100
+vcfMap[vcfMap < 0] <- 0
+plot(vcfMap)
+
+# Calculate the RMSE 
+RMSE <- sqrt(mean((alldata$VCF-vcfMap)^2))
+hist(RMSE)
+
+# Load training zones
